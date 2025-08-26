2f24b556ad8ba6c7a275fae1052194c91653f995
Erick244 <erienrique03@gmail.com>
2025-08-26 15:47:31 -0300

    Refatora o componente HeroSection, melhorando a animação de desvanecimento e ajustando a responsividade. Atualiza o layout da página inicial para otimizar a experiência do usuário em dispositivos móveis.

---
+++ src/data/techs.data.tsx
@@ -0,0 +1,9 @@
+import ReactIcon from "@/components/icons/ReactIcon";
+
+export const techs = [
+    {
+        node: <ReactIcon />,
+        title: "React",
+        href: "https://react.dev/",
+    },
+];
